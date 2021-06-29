a7418438128581062a7d61c9f8daf6c3b1ba464a
PranshuSingh11 <57186431+PranshuSingh11@users.noreply.github.com>
2021-06-29 10:46:08 +0530

    Update App.js

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -46,7 +46,7 @@ function App() {
         <ProtectedRoute path='/profile' exact component={Profile}></ProtectedRoute>
         <ProtectedRoute path='/logout' exact component={Logout}></ProtectedRoute> 
         <Route path='/unauthorized' exact ><Unauthorized></Unauthorized></Route>
-        <Route path='*' >404 Page Not found</Route>
+        <Route path='*' >40 Page Not found</Route>
       </Switch>
     </div>
     </Router>
